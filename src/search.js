a77b03996649819c2b96a4d5e347bbd6cf7ff1da
TeoJJss <teojunjia@gmail.com>
2023-09-02 09:51:38 +0800

    contact

--- src/search.js
+++ src/search.js
@@ -2,11 +2,14 @@ function searchFunction(type) {
     var inputElement, body;
     
     if (type == 'product') {
-        inputElement = document.getElementsByClassName('search-input')[0];
+        inputElement = document.getElementsByClassName('product-content-search-input')[0];
         body = document.getElementsByClassName('product-table')[0];
     } else if (type == 'user') {
         inputElement = document.getElementsByClassName('user-content-search-input')[0];
         body = document.getElementsByClassName('user-table')[0];
+    }else{
+        inputElement = document.getElementsByClassName('search-input')[0];
+        body = document.getElementsByClassName('table')[0];
     }
     
     var input = inputElement.value.toUpperCase();
