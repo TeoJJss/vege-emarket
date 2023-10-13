853132d5e9003fbcdf5505f537432c00f39074da
zishengg <129036052+zishengg@users.noreply.github.com>
2023-10-13 23:23:02 +0800

    first

---
+++ consumer/homepage.php
@@ -0,0 +1,22 @@
+<?php
+    require "../modules/config.php";
+    if ($_SERVER['REQUEST_METHOD']=='POST'){
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HomePage</title>
+    <link rel="stylesheet" href="../styles/title.css">
+    <style>
+    
+    </style>
+</head>
+
+<body>
+
+</body>
