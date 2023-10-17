f24955703558fe9c09a5c4797e6843b419070043
TeoJJss <teojunjia@gmail.com>
2023-10-17 20:09:49 +0800

    Error handler title

--- modules/err_handler.php
+++ modules/err_handler.php
@@ -1,6 +1,7 @@
 <?php 
     function err_handler($err_code, $err_msg, $file_dir, $line_no){
         $filename= basename($file_dir); //Get the filename from file directory
+        echo "<title>Error!</title>";
         echo "<center style='font-family: Arial'><h1 style='color:red;'>Something went wrong!</h1> <br> $err_code $err_msg";
         echo "<p>at $filename, line $line_no</p><br>";
 
